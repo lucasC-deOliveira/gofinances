4066101911ceab1c4a698373fc2446532ef0d51c
Lucas Carvalho <67061874+lucasC-deOliveira@users.noreply.github.com>
2022-07-16 22:38:21 -0300

    Utilizando Fontes Personalizadas

--- App.tsx
+++ App.tsx
@@ -2,9 +2,28 @@
 import React from 'react';
 import { Dashboard } from './src/screens';
 import { ThemeProvider } from 'styled-components';
+import AppLoading from 'expo-app-loading';
+
 import theme from './src/global/styles/theme';
 
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from "@expo-google-fonts/poppins"
+
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
   return (
     <ThemeProvider theme={theme}>
       <Dashboard />
