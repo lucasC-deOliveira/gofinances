e2a711abe9c8841cf623e6f7265a2a9ffa318b82
Lucas Carvalho <67061874+lucasC-deOliveira@users.noreply.github.com>
2022-10-13 13:33:45 -0300

    Mocks Personalizados

--- src/hooks/auth.spec.tsx
+++ src/hooks/auth.spec.tsx
@@ -1,32 +1,24 @@
 import { renderHook, act } from "@testing-library/react-hooks"
 import { AuthProvider, useAuth } from "./Auth"
+import { mocked } from 'ts-jest/utils'
+import { logInAsync } from 'expo-google-app-auth'
 
-jest.mock('expo-auth-session', () => {
-  return {
-    startAsync: () => {
-      return {
-        type: 'success',
-        params: {
-          access_token: 'google-token '
-        }
-      }
-    }
-  }
-})
+jest.mock('expo-google-app-auth')
 
 describe('Auth Hook', () => {
   it('should be able to sign in with existing google account', async () => {
+    const googleMocked = mocked(logInAsync as any)
 
-    global.fetch = jest.fn(() => Promise.resolve({
-      json: () => Promise.resolve({
-        id: `userInfo.id`,
-        email: `userInfo.email`,
-        name: `userInfo.given_name`,
-        photo: `userInfo.picture`,
-        locale: `userInfo.locale`,
-        verified_email: `userInfo.verified.email`
-      })
-    })) as jest.Mock
+    googleMocked.mockedReturnValue({
+
+      type: "success",
+      user: {
+        id: "any_id",
+        email: "lucas@gmail.com",
+        name: "Lucas",
+        photo: "any_photo.png"
+      }
+    })
 
     const { result } = renderHook(() => useAuth, {
       wrapper: AuthProvider
@@ -34,7 +26,24 @@ describe('Auth Hook', () => {
 
     await act(() => result.current.signInWithGoogle());
 
-    expect(result.current.user).toBeTruthy();
+    expect(result.current.user.email).toBe('lucas@gmail.com');
+
+  });
+
+  it('user should not connect if cancel authentication with google', async () => {
+    const googleMocked = mocked(logInAsync as any)
+
+    googleMocked.mockedReturnValue({
+      type: "cancel"
+    })
+
+    const { result } = renderHook(() => useAuth, {
+      wrapper: AuthProvider
+    });
+
+    await act(() => result.current.signInWithGoogle());
+
+    expect(result.current.user.email).not.toHaveProperty('id')
 
   });
 });
